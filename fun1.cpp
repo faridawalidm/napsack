db9e467cac351fe2294bc4c4f8e78e5a8237c42d
MohamedElsayed006 <159177483+MohamedElsayed006@users.noreply.github.com>
2024-02-11 12:36:24 +0200

    Update fun1.cpp

--- fun1.cpp
+++ fun1.cpp
@@ -20,4 +20,14 @@ return knapSack(W, wt, val, n - 1);
 else
 return max(val[n - 1] + knapSack(W - wt[n - 1], wt, val, n - 1), knapSack(W, wt,
 val, n - 1));
+
+  int main()
+{
+int val[] = { 60, 100, 120 };
+int wt[] = { 10, 20, 30 };
+int W = 50;
+int n = sizeof(val) / sizeof(val[0]);
+cout << knapSack(W, wt, val, n);
+return 0;
+}
 }
