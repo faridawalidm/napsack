2b1a1f83d573b454b795b810295fb5f68f24e170
MohamedElsayed006 <mohamedelsayed006@aucegypt.edu>
2024-02-13 20:17:06 +0200

    k

--- fun1.h
+++ fun1.h
@@ -1,9 +1,4 @@
-int main()
-{
-int val[] = { 60, 100, 120 };
-int wt[] = { 10, 20, 30 };
-int W = 50;
-int n = sizeof(val) / sizeof(val[0]);
-cout << knapSack(W, wt, val, n);
-return 0;
-}
+# include <iostream>;
+using namespace std;
+int max(int a, int b);
+int KnapSack(int W, int wt[], int val[],int n);
